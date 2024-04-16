3ce19cba168e128ff56fee02f9ab143a36d48011
sshhhgit <seungho0497@hanmail.net>
2024-04-16 09:05:55 +0900

    Function props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import Todos from './components/Todo';
+import { useState } from 'react';
+
+import NewTodo from './components/NewTodo';
+import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+     });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos} />
     </div>
   );
