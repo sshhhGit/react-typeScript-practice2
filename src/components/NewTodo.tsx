eb7b261c962f47b756bdf0408a82482b71266352
sshhhgit <seungho0497@hanmail.net>
2024-04-17 08:51:51 +0900

    typeScript + useContext

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,6 +1,11 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 
-const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
+import { TodosContext } from "../store/todos-context";
+import classes from './NewTodo.module.css';
+
+const NewTodo: React.FC = () => {
+    const todosCtx = useContext(TodosContext);
+
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
@@ -13,11 +18,11 @@ const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
             return;
         }
 
-        props.onAddTodo(enteredText);
+        todosCtx.addTodo(enteredText);
     };
 
     return (
-      <form onSubmit={submitHandler}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <label htmlFor="text">Todo text</label>
             <input type="text" id='text' ref={todoTextInputRef} />
         <button>Add Todo</button>
